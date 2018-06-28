205f065070e3dc3a0e224c965506bfd480d12e04
Philip Bowman <philnbowman@gmail.com>
2018-06-27 21:22:47 -0600

    Organized and added comments to main.cc.

--- main.cc
+++ main.cc
@@ -1,42 +1,41 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 using namespace std;
 
-void sieve(int n) { // Sieve of Eratosthenes
+/* Sieve of Eratosthenes */
+void sieve(int n) {
   if(n > 1) {
     bool a[n+1];
 
     for(int i=2; i <= n; i++) {
-      a[i]=true;
+      a[i]=true; // set all elements to true
     }
     
     for(int i=2; i <= sqrt(n); i++) {
       if(a[i] == true) {
 	for(int j=i*i; j <= n; j += i) {
-	  a[j]=false;
+	  a[j]=false; // set all composite numbers to false
 	}
       }
     }
     
     for(int i=2; i <= n; i++) {
       if(a[i] == true) {
-	cout << i << endl;
+	cout << i << endl; // print primes to stdout
       }
     }
-
   } else {
-    cerr << "Error: \"n\" needs to be an integer greater than 1.";
+    cerr << "Error: 'n' needs to be an integer greater than 1.";
     exit(1);
   }
 }
 
-int main(int argc,const char *argv[]) {
+int main() {
   int n;
 
   cout << "Number: ";
-  cin >> n;
+  cin >> n;  // user input
   
   sieve(n);
 
